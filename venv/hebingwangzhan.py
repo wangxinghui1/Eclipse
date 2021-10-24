cf79e1e595b692573ca361fd36594c341f9fb87e
wangxinghui1 <yuepohualing@gmail.com>
2021-10-24 08:51:11 -0700

    hebing

--- venv/hebingwangzhan.py
+++ venv/hebingwangzhan.py
@@ -1,15 +1,15 @@
 import pandas as pd
 from pathlib import Path
 #选择合并路径
-EX_dir=Path("C:\Ex")
+EX_dir=Path("/Users/wangxinghui/Downloads/STO")
 #利用glob方法获取所有Excel list
 ex_files=EX_dir.glob('*.xlsx')
 #创建pandas表格容器dataFrame
 df=pd.DataFrame()
 #遍历Excel文件，读取表格数据添加到pandas中
 for xlsx in ex_files:
-   data=pd.read_excel(xlsx,)
+   data=pd.read_excel(xlsx)
    #不断的读取并添加数据
    df=df.append(data)
 #将所有数据写入到新的数据中去
-df.to_excel(EX_dir,"OB.xlsx")
+df.to_excel('/Users/wangxinghui/Downloads/STO/excel1.xlsx')
